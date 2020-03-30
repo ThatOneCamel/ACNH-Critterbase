7f64e110bb2da192085514f85789e5f44c3367df
TCameronT <thomascameront@gmail.com>
2020-03-30 11:51:03 -0500

    Updated App Icon

--- Client/src/components/Toolbar.js
+++ Client/src/components/Toolbar.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from 'react-bootstrap/Navbar'
 import bellImage from '../images/bells_big.png'
+import '../Palette.css'
 
 function Toolbar(){
     return(
-        <Navbar bg="light" expand="lg">
+        <Navbar bg="primary" variant='dark' expand="sm" fixed="top">
             <Navbar.Brand>
                 <img src={bellImage} width="30" height="30" alt="Bells Icon" />
+                Seasonpedia
             </Navbar.Brand>
         </Navbar>
     )
